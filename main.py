1bda816e921ceb81e468de80e3c431395957ebca
STANEEER <sgozdzikowski@st.swps.edu.pl>
2024-02-06 23:13:40 +0100

    Strona została postawiona i działa

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, url_for, request
+import functions_framework
 
 
 app = Flask(__name__)
 
 
+# todo: Połączenie z bazą danych GCP
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         url = request.form['url_text']
-        return 'Hello'
+        return url
     else:
         return render_template('index.html')
 
